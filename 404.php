6bb2d575eed0adaee67ae083a80159ee79cd5564
Filip Kowal <filip.piotr.kowal@gmail.com>
2020-07-16 04:22:29 +0200

    Create 404 page

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>://404</title>
+    <link rel="stylesheet" href="./styles/base.scss">
+    <link href="./styles/404.scss" rel="stylesheet">
+    </style>
+</head>
+
+<body>
+    <video class="noise" alt="White Noise GIF by Justin"
+        src="https://media2.giphy.com/media/YRcXl6VfNhCorklI0R/giphy.mp4?cid=ecf05e4736ac91375dfc295b6e3a9251d97150d3f704d298&amp;rid=giphy.mp4"
+        poster="https://media2.giphy.com/media/YRcXl6VfNhCorklI0R/giphy_s.gif?cid=ecf05e4736ac91375dfc295b6e3a9251d97150d3f704d298&amp;rid=giphy_s.gif"
+        autoplay="" loop="" playsinline="">
+
+    </video>
+    <a class="no-decoration" href="index.php">
+        <span class="logo-sign no-decoration">://</span>
+    </a>
+</body>
+
+</html>
